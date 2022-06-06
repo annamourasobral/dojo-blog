e618e67c00939a6078e56b2b3ab21f31c0446708
Anna Moura Sobral <annamourasobral@gmail.com>
2022-06-06 19:00:05 +0200

    ading params to routing to access the content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './NavBar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
           </Routes>
         </div>
       </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
     const [data, setData] = useState(null);
@@ -6,25 +6,37 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('could not fetch the date for that resource');
-                } else {
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if (!res.ok) { // error coming back from server
+                        throw Error('could not fetch the data for that resource');
+                    }
                     return res.json();
-                }
-            })
-            .then((data) => {
-                setData(data);
-                setLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                setLoading(false);
-                setError(err.message);
-            })
-    }, [url]);
-    return { data, isLoading, error }
+                })
+                .then(data => {
+                    setLoading(false);
+                    setData(data);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted')
+                    } else {
+                        // auto catches network / connection error
+                        setLoading(false);
+                        setError(err.message);
+                    }
+                })
+        }, 1000);
+
+        // abort the fetch
+        return () => abortCont.abort();
+    }, [url])
+
+    return { data, isLoading, error };
 }
- 
+
 export default useFetch;
